49f730f45adc8e1196027b041e6f38c4db303ed3
NoraPernyesz82 <noxpnk@gmail.com>
2019-12-05 17:26:03 +0100

    Első feltöltés

---
+++ hegyek.js
@@ -0,0 +1,47 @@
+"use strict";
+
+let hegyek = [];
+let app = angular.module('myApp', []);
+
+app.controller('myCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('hegyekMo.txt')
+    .then(function(response){
+        $scope.szummamagassag = 0;
+        $scope.legmagasabbhegynNev = "";
+        $scope.legmagasabbhegyCsucs = "";
+        $scope.legmagasabbhegyHegyseg = "";
+        $scope.legmagasabbhegyMagassag = "";
+        $scope.adatok = response.data;
+        $scope.tomb = $scope.adatok.split('\n');
+        for (let i = 1; i < $scope.tomb.length; i++) { 
+            hegyek.push({
+                'hegycsucs' : $scope.tomb[i].split(';')[0],
+                'hegy' : $scope.tomb[i].split(';')[1],
+                'magassag' : $scope.tomb[i].split(';')[2],
+            });
+            let magassag = Number($scope.tomb[i].split(';')[2]);
+            $scope.szummamagassag += Number($scope.tomb[i].split(';')[2]);
+            if ($scope.legmagasabbhegyMagassag < magassag) {
+                $scope.legmagasabbhegyMagassag = magassag;
+                $scope.legmagasabbhegynNev =  $scope.tomb[i].split(';')[0];
+                $scope.legmagasabbhegyHegyseg = $scope.tomb[i].split(';')[1];
+            }
+        }
+        $scope.darabszam = hegyek.length;
+        $scope.atlagmagassag = $scope.szummamagassag / $scope.darabszam;
+    });
+}]);
+
+app.controller('myControl', ['$scope', function($scope){
+    $scope.leker = function(){
+        let magassag = document.getElementById('magassag').value;
+    let vane = false;
+    for (let i = 0; i < hegyek.length; i++) {
+        if (hegyek[i].hegy == "Börzsöny" && Number(hegyek[i].magassag) > magassag) {
+            vane = true;
+            break;
+        }
+    }
+    $scope.kiir = (vane ? `Van ${magassag}m-nél magasabb hegycsúcs a Börzsönyben` : `Nincs ${magassag}m-nél magasabb hegycsúcs a Börzsönyben`);
+}
+}]);
